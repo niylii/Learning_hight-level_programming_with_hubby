36022656e21929088f5cd4099492236308708ff6
nouhailaaleen <173621209+niylii@users.noreply.github.com>
2025-02-06 21:05:26 +0100

    Cleopatra captivated Caesar, shaping the Roman Empire *smirk*

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    romans = {"I" : 1, "V" : 5, "X": 10, "L": 50, "C": 100, "D" : 500, "M": 1000}
+    romans = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
     number = 0
     prev = 0
     if roman_string is None:
         return 0
     for i in roman_string[::-1]:
         curr = romans[i]
-        if curr < prev: 
+        if curr < prev:
             number -= curr
         else:
             number += curr
